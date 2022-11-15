21aea255edecfa68314e12ba3b88f908661143f7
Jorrit van der Plaats <jvdplaats@hotmail.com>
2022-11-15 14:30:24 +0100

    crud in rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "articles#index"
+  # root "articles#index"
+  get '/tasks', to: 'tasks#index'
 
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  post '/tasks', to: 'tasks#create'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/task/:id', to: 'tasks#destroy', as: 'task_delete'
 end
